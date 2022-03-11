f89177b7f493b55000e1511dfbf2b2b7413d2471
ChrisBob1 <98698407+ChrisBob1@users.noreply.github.com>
2022-03-11 10:49:05 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'bodacious'
+game 'gta5'
+
+ -- Head over to https://docs.fivem.net/game-references/data-files/ for the data files
+ -- REMEMBER TO REMOVE THE FILE FROM files and data_file IF NOT IN RESOURCE!!
+ 
+ files {
+	'contentunlocks.meta',
+	'loadouts.meta',
+	'pedpersonality.meta',
+	'shop_weapon.meta',
+	'weaponanimations.meta',
+	'weaponarchetypes.meta',
+	'weapons.meta'
+	}
+
+	data_file 'WEAPONINFO_FILE' 'weapons.meta'
+	data_file 'WEAPON_METADATA_FILE' 'weaponarchetypes.meta'
+	data_file 'WEAPON_SHOP_INFO' 'shop_weapon.meta'
+	data_file 'WEAPON_ANIMATIONS_FILE' 'weaponanimations.meta'
+	data_file 'CONTENT_UNLOCKING_META_FILE' 'contentunlocks.meta'
+	data_file 'LOADOUTS_FILE' 'loadouts.meta'
+	data_file 'PED_PERSONALITY_FILE' 'pedpersonality.meta'
